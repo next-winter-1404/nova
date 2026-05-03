534ad411d6c85e185b5b1637d7828f9c8f002899
BaharVahabi <bahar.vhbii@gmail.com>
2026-05-03 03:37:05 +0330

    style structure

--- src/app/(main)/housereserve/page.tsx
+++ src/app/(main)/housereserve/page.tsx
@@ -1,50 +1,46 @@
 "use client";
-import Button from "@/src/components/common/button/page";
-import DropMenu from "@/src/components/common/dropMenu/DropMenu";
-import Input from "@/src/components/common/input/Input";
 import { useState } from "react";
-import megaPhone from "@/src/assets/icons/megaphone.svg";
-import Image from "next/image";
+import DropMenu from "@/src/components/common/dropMenu/DropMenu";
+import TopReserveHouseSection from "@/src/components/reserveHouse/topSection";
+import Input from "@/src/components/common/input/Input";
 
 const HouseReservePage = () => {
-  const [checkOutDate, setCheckOutDate] = useState("nyc");
-  const [checkInDate, setCheckInDate] = useState("nyc");
-
-  const cities = [
-    { value: "nyc", label: "New York" },
-    { value: "la", label: "Los Angeles" },
-    { value: "chi", label: "Chicago" },
+  const [facil, setFacil] = useState("room");
+  const Facilities = [
+    {
+      value: "room",
+      label: "room",
+    },
+    {
+      value: "bath",
+      label: "bath",
+    },
+    {
+      value: "balcony",
+      label: "balcony",
+    },
   ];
   return (
-    <div className="mt-15 bg-dark-800 flex-between rounded-3xl p-4" dir="rtl">
-      <div className="flex justify-evenly w-4/5">
-        <DropMenu
-          options={cities}
-          value={checkInDate}
-          onChange={setCheckInDate}
-          placeholder="وارد کنید ..."
-          dropDownLabel=": تاریخ ورود "
-        />
-        <DropMenu
-          options={cities}
-          value={checkOutDate}
-          onChange={setCheckOutDate}
-          placeholder="وارد کنید ..."
-          dropDownLabel=":  تاریخ خروج"
-        />
-        <Input
-          labelText="جستجو"
-          InputHeight="h-[50px]"
-          tagBgStyle={{ background: "var(--color-dark-800)" }}
-        />
+    <>
+      <TopReserveHouseSection />
+      <div className="bg-dark-800 flex mt-12 p-6">
+      <div>map</div>
+
+        <div className="w-[60%]">
+          <div className="flex">
+            <DropMenu
+              options={Facilities}
+              dropDownLabel="امکانات هتل"
+              onChange={setFacil}
+              value={facil}
+            />
+            <Input labelText="حداقل قیمت" tagBgStyle={{background:"var(--color-dark-800)"}}/>
+            <Input labelText="حداکثر قیمت" tagBgStyle={{background:"var(--color-dark-800)"}}/>
+          </div>
+          <div>bottom</div>
+        </div>
       </div>
-      <Button
-        text={`تعداد اگهی : ${33}`}
-        backgroundColor="transparent"
-        buttonStyle={{ background: "transparent", border: "1px solid white" }}
-        icon={<Image alt="megaphone" src={megaPhone} />}
-      />
-    </div>
+    </>
   );
 };
 
