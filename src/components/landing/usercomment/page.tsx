19525f17593ec9e8e995d93affd9f82f06ad9d53
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2026-05-06 01:18:49 +0330

    finish responsive

--- src/components/landing/usercomment/page.tsx
+++ src/components/landing/usercomment/page.tsx
@@ -12,7 +12,7 @@ const UserComment = () => {
                 <span className ='md:text-[32px] text-[22px] text-white-pure'> !رضایت شما اعتبار و ارزش دلتا را می سازد </span>
                 <h2 className='md:text-[16px] text-[14px] text-right text-white-pure md:w-full w-[260px]'> .تیم دلتا با ارائه بهترین نیرو های خدماتی و سرویس های املاکی سعی دارد تا بتواند در تمام لحظات کنار شما باشد </h2>
             </div>
-            <div className ='w-[390px] md:w-[1376px] flex md:flex-row flex-col gap-5 md:h-[310px]'>  
+            <div className ='w-[390px] md:w-[1376px] flex md:flex-row items-center flex-col gap-5 md:h-[310px]'>  
                 <div><CommentCard></CommentCard></div>                         
                 <div><CommentCard></CommentCard></div>
             </div>
