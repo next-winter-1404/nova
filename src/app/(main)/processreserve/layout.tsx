b2868074e71c65ac25d1d4624debbb71f280a770
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2026-05-09 05:52:15 +0330

    finish res

--- src/app/(main)/processreserve/layout.tsx
+++ src/app/(main)/processreserve/layout.tsx
@@ -27,33 +27,33 @@ const Layout = ({children} : IProps) => {
   return ( 
     <div className='bg-dark-900 w-full'>
         <div className='flex flex-col w-full justify-center items-center mt-[172px] ' dir='rtl'>
-            <div className='w-11/12 h-[68px]  border-amber-50 rounded-3xl flex items-center justify-center bg-dark-700'>
+            <div className='md:w-11/12 w-[340px] h-[68px] border-amber-50 rounded-3xl flex items-center justify-center bg-dark-700'>
                 <div className='w-22/23 h-[45px] flex items-center justify-between relative'>
-                    <div className='flex gap-4 items-center'>
-                        <div className ='w-9 h-9 rounded-full bg-primary-accent-green flex items-center justify-center'>
+                    <div className='flex md:gap-4 gap-1.5 items-center'>
+                        <div className ='md:w-9 w-5 md:h-9 h-5 rounded-full bg-primary-accent-green flex items-center justify-center'>
                             {<Image src={hotel} alt='hotel'/>}
                         </div>
-                        <span className='text-[20px] text-primary-accent-green'>انتخاب هتل</span>
+                        <span className='text-[12px] md:text-[20px] text-primary-accent-green'>انتخاب هتل</span>
                     </div>
-                    <div className='border-b-3 w-[112px] border-dashed border-primary-accent-green'></div>    
+                    <div className='hidden md:block border-b-3 w-[112px] border-dashed border-primary-accent-green'></div>    
                     {steps.map((step, index) => {
                         const isCompleted = currentIndex > index ;
                         const isActive = currentIndex === index;
                         return(
-                            <div key={step.id} className='flex items-center gap-4'>
-                                <div className ={`w-9 h-9 rounded-full bg-primary-accent-green flex items-center justify-center ${
+                            <div key={step.id} className='flex items-center gap-1.5 md:gap-4'>
+                                <div className ={`md:w-9 w-6 md:h-9 h-6 rounded-full bg-primary-accent-green flex items-center justify-center ${
                                     isCompleted ? 'bg-primary-accent-green' : isActive ? 'border border-white-pure bg-transparent' : 'border border-gray-300 bg-transparent'
                                     }`}
                                     >
                                     <Image src={stepIcons[index]} alt={step.label} width={16} height={16}/>
                                 </div>
-                                <span className ={`text-[20px] ${
+                                <span className ={`text-[12px] md:text-[20px] ${
                                     isCompleted ? 'text-primary-accent-green' : isActive ? 'text-white-pure' : 'text-gray-300'
                                 }`}>
                                     {step.label}
                                 </span>
                                 {index < steps.length -1 && (
-                                    <div className={`border-b-3 w-[112px] border-dashed ${
+                                    <div className={`hidden md:block border-b-3 w-[112px] border-dashed ${
                                         isCompleted ? 'border-primary-accent-green' : isActive ? 'border-white-pure' : 'border-gray-300'
                                     }`}>
                                     </div>
