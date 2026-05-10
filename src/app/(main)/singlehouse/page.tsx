ec044c6843fa9f3f204681cbdd42aa91603c5baf
aminasama <236314759+aminasamacreator-byte@users.noreply.github.com>
2026-05-10 06:54:06 -0700

    add container

---
+++ src/app/(main)/singlehouse/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Container from "@/src/components/common/Container"
+import { Breadcrumb, BreadcrumbItem } from "@/src/components/common/breadCrumbs";
+import HousePicture from "@/src/assets/images/singleHouse.png"
+
+const SingleHousePage = () => {
+  const items:BreadcrumbItem[] = [
+    {
+      href: "/reserve-house",
+      label: "رزرو هتل",
+    },
+    {
+      label: `رزرو هتل ${"رشت"}`,
+    },
+
+  ];
+  return (
+    <div className="padding-section flex-col-center sm:mt-24 mt-15 bg-tomato-red">
+        <div className="flex-col-center sm:gap-6 gap-3">
+            <Breadcrumb items={items} twClassname="bg-white-pure w-full flex-center justify-start"/>
+            <div className="flex-center justify gap-5">
+                <div className="flex flex-1">LeftOne</div>
+                <div className="flex-col-center flex-1 rounded rounded-[32px]">
+                    <div className="rounded rounded-[32px]"><Image src={HousePicture} alt="home pic"/></div>
+                    <div className="relative rounded rounded-[32px] bg-dark-900-transparent-64">
+                        <Image src={HousePicture} alt="home pic"/>
+                        <span> +12 عکس دیگر </span>
+                    </div>
+                </div>
+                <div className="flex flex-2 rounded rounded-[32px] shadow-000-8">
+                    <Image src={HousePicture} alt="home pic"/>
+                </div>
+            </div>
+        </div>
+      
+      <Container>
+      </Container>
+    </div>
+  )
+}
+
+export default SingleHousePage
