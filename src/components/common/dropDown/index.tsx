88e696cad5feac6f1fa0729bf9846bac4a8e1d46
BaharVahabi <bahar.vhbii@gmail.com>
2026-05-12 01:46:44 +0330

    a littile change in drop daown style

--- src/components/common/dropDown/index.tsx
+++ src/components/common/dropDown/index.tsx
@@ -35,15 +35,15 @@ const SimpleDropdown: FC<IDropDownProps> = ({
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild className="border">
-        <button className={`IconButton dropMenu p-5  relative ${triggerClassName}`}  aria-label="Customise options">
+        <button className={`IconButton dropMenu p-5  relative  ${triggerClassName}`}  aria-label="Customise options" dir="rtl">
           <span>{showSelected()}</span>
-          <span className={`absolute -top-3 p-0.5 px-2 right-3.5 ${tagBg}`}>:{labelText}</span>
+          <span className={`absolute -top-3 p-0.5 px-2 right-3.5 ${tagBg}`}>{labelText}:</span>
 
         </button>
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
-        <DropdownMenu.Content className="DropdownMenuContent w-full p-4 rounded rounded-xl z-100 bg-dark-800">
+        <DropdownMenu.Content className="DropdownMenuContent w-full p-4 rounded-xl z-100 bg-dark-800 text-white cursor-pointer">
           {options.map((op) => (
             <DropdownMenu.Item
               key={op.value}
@@ -52,7 +52,7 @@ const SimpleDropdown: FC<IDropDownProps> = ({
             >
               {op.label}
               {selectedValue === op.value && (
-                <span className="text-blue-500 text-xs">✓</span>
+                <span className="text-blue-500 text-xs mx-2">✓</span>
               )}
             </DropdownMenu.Item>
           ))}
--- src/components/reserveHouse/bottomNavbarFilter.tsx
+++ src/components/reserveHouse/bottomNavbarFilter.tsx
@@ -12,7 +12,7 @@ const Facilities = [
   },
   {
     value: "area",
-    label: "منظقه",
+    label: "منطقه",
   },
 ];
 const BottomNavbarFilter = () => {
