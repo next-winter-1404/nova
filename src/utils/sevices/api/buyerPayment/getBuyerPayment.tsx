d56a817faef7f9d9ec27963fe7498d59df68ee68
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2026-05-24 15:24:48 +0330

    pagination

--- src/utils/sevices/api/buyerPayment/getBuyerPayment.tsx
+++ src/utils/sevices/api/buyerPayment/getBuyerPayment.tsx
@@ -1,8 +1,9 @@
 "use server"
 
+import { IBuyerPayList } from "@/src/core/types/IBuyerPay";
 import instance from "../../interseptor"
 
-export const getBuyerPayment = async (apiParams = {}) => {
+export const getBuyerPayment = async (apiParams = {}) : Promise<IBuyerPayList> => {
     const response = await instance.get("api/payments",{
         params : apiParams,
     });
