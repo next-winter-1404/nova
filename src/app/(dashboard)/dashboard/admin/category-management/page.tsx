ad9b1d8d5e76113c5637448a87a04632f45638e5
BaharVahabi <bahar.vhbii@gmail.com>
2026-06-03 15:12:27 +0330

    add total count for reserve

---
+++ src/app/(dashboard)/dashboard/admin/category-management/page.tsx
@@ -0,0 +1,13 @@
+import DashboardContentContainer from '@/src/components/common/dashboardcontentcontainer/container'
+import React from 'react'
+
+const CategoryManagementPage = () => {
+  return (
+
+      <DashboardContentContainer>
+        
+      </DashboardContentContainer>
+  )
+}
+
+export default CategoryManagementPage
