a33d9556bcc42bd933a5fa08cc52bc3bd6aefec1
BaharVahabi <bahar.vhbii@gmail.com>
2026-06-13 20:21:13 +0330

    add loading admin  dashboard

---
+++ src/app/(dashboard)/dashboard/admin/qa-management/loading.tsx
@@ -0,0 +1,10 @@
+import SpinnerComponent from '@/src/components/spinner/Spinner'
+import React from 'react'
+
+const loading = () => {
+  return (
+<SpinnerComponent/>
+  )
+}
+
+export default loading
