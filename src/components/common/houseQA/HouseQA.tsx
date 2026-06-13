62ae4ee0f510d48960e8a7477927d10eecf7d3d6
BaharVahabi <bahar.vhbii@gmail.com>
2026-06-13 22:35:02 +0330

    fix QA section

--- src/components/common/houseQA/HouseQA.tsx
+++ src/components/common/houseQA/HouseQA.tsx
@@ -3,6 +3,7 @@ import AccordionComponent from "../accardeon/Accardeon";
 import { getHouseQA } from "@/src/utils/sevices/api/QA/gettHouseQA";
 import { Modal } from "../modal";
 import SendQAQuestion from "../../reserveHouse/sendQAQuestion/SendQAQuestion";
+import HouseQAList from "./HouseQAList";
 
 interface Props {
   houseId: number;
@@ -10,35 +11,13 @@ interface Props {
 const HouseQA: FC<Props> = async ({ houseId }) => {
   const houseQA = await getHouseQA(houseId);
   return (
-    <div className="flex flex-col gap-10 text-white" dir="rtl">
+    <div className="flex flex-col gap-10 text-white-pure" dir="rtl">
       <h1 className="font-semibold text-xl">
         پیش از رزرو یا خرید این ملک، ممکن است سوالاتی برای شما مطرح شود. در
         ادامه پاسخ برخی از پرتکرارترین سوالات کاربران را مشاهده می‌کنید.
       </h1>
 
-      <div className="flex flex-col gap-8">
-        {houseQA.length > 0 ? (
-          houseQA
-            .slice(0, 4)
-            .map((QA) => (
-              <AccordionComponent
-                twHeaderClassName="text-gray-300 cursor-pointer"
-                key={QA.id}
-                twContentClassName="mt-3"
-                twTitleClassName="text-[18px]"
-                isLine={false}
-                accordionTitle={QA.question || ""}
-                accordionContent={
-                  QA.answer || "در حال حاضر پاسخی برای این سوال وجود ندارد"
-                }
-              />
-            ))
-        ) : (
-          <div className="w-full text-center text-gray-300  text-3xl">
-            سوالی برای این ملک وجود ندارد
-          </div>
-        )}
-      </div>
+      <HouseQAList houseQA={houseQA} />
       <Modal
         modalTitle="سوالی دارید؟"
         contentClassName="bg-dark-900 text-white"
