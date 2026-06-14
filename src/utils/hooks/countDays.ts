e6c6f8d319dde45d3924c49db103d798b663c1df
BaharVahabi <bahar.vhbii@gmail.com>
2026-06-14 14:33:06 +0330

    fix nan problem in admin dashboard

--- src/utils/hooks/countDays.ts
+++ src/utils/hooks/countDays.ts
@@ -1,10 +1,19 @@
-export const calculateDaysBetween = (startDate?: string, endDate?: string) => {
-  if (!startDate || !endDate) return 0;
-    
-  const start = new Date(startDate);
-  const end = new Date(endDate);
-    if (end <= start) return 0;
-    const diffTime = Math.abs(end.getTime() - start.getTime());
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    return diffDays;
-  };
+export const calculateDaysBetween = (
+  startDate?: string,
+  endDate?: string
+) => {
+  const start = new Date(startDate ?? "");
+  const end = new Date(endDate ?? "");
+
+  if (
+    isNaN(start.getTime()) ||
+    isNaN(end.getTime()) ||
+    end <= start
+  ) {
+    return 0;
+  }
+
+  return Math.ceil(
+    (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24)
+  );
+};
